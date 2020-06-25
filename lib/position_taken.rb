4202d66fb5b9d5d37bff5d4583c0300389a6006a
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-25 00:57:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
-def position_taken?(board, index, current_player = “X” || “O”)
-	if board[index] != “ “ && “” do
+def position_taken?(board, index)
+	(board[index] != “ “ && “” do
 		false
 	else
 		true
