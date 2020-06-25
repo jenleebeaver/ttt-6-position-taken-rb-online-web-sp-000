87726cb8c2da22bbe842d99ab1ae3b0372abd187
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-25 01:02:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-	(board[index] == “ “ || board[index] == “” || board[index] == nil) ?
-	false : true
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+  false : true
 end
 
 # taken = nil
